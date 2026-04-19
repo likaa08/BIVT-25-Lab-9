407c6a9ac8fc854935992405b90c698bf1511e3f
likaa08 <14angel_v14@mail.ru>
2026-04-19 17:44:22 +0300

    Update Green.cs

--- Lab9/Green/Green.cs
+++ Lab9/Green/Green.cs
@@ -1,3 +1,24 @@
-﻿namespace Lab9.Green
+namespace Lab9.Green
 {
+    public abstract class Green
+    {
+        private string _input;
+        public string Input => _input;
+        protected static char[] _punc;
+        protected Green(string input)
+        {
+            _input = input;
+        }
+        static Green()
+        {
+            _punc = new char[] { '.', '!', '?', ',', ':', '\"', ';', '–', '(', ')', '[', ']', '{', '}', '/' };
+        }
+        public abstract void Review();
+        public virtual void ChangeText(string input)
+        {
+            _input = input;
+            Review();
+        }
+    }
 }
+
